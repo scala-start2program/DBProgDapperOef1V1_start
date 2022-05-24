7d88b27cbeb875270bd88dec9c03fa18bc7de2e3
jandedeurwaerder-howest <jan.de.deurwaerder@howest.be>
2022-05-24 11:40:06 +0200

    Add project files.

---
+++ Scala.Gezelschapsspelen.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Scala.Gezelschapsspelen.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        private void LstSpelen_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        private void BtnToevoegen_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnWijzigen_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnVerwijderen_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnBewaren_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnAnnuleren_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
